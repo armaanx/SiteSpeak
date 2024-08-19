67d995c4eb152d5986b745ad999fb0fb2594df89
Armaan Mishra <armaanmishra48@gmail.com>
2024-08-19 19:24:37 +0530

    add nextui provider

---
+++ src/components/Providers.tsx
@@ -0,0 +1,5 @@
+import { NextUIProvider } from "@nextui-org/react";
+
+export const Providers = ({ children }: { children: React.ReactNode }) => {
+  return <NextUIProvider>{children}</NextUIProvider>;
+};
